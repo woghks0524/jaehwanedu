16ad220ecb5fde8a0fd944a8492ea24c423651f0
woghks0524 <woghks0524jjh@gmail.com>
2024-10-06 15:24:04 +0900

    Add files via upload

--- studentpage.py
+++ studentpage.py
@@ -26,12 +26,11 @@ st.set_page_config(layout="wide")
 hide_streamlit_style = """
             <style>
             MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-# header {visibility: hidden;}
 
 # 세션 상태 초기화
 if 'page' not in st.session_state:
